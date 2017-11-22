313af09601685281c25e0863affffb2a8e4d9f7c
aaric <vipaaric@gmail.com>
2017-11-22 18:07:34 +0800

    协议解析器空壳

---
+++ src/test/java/com/incarcloud/rooster/datapack/DataParserGmmcTest.java
@@ -0,0 +1,17 @@
+package com.incarcloud.rooster.datapack;
+
+import org.junit.Test;
+
+/**
+ * DataParserGmmcTest
+ *
+ * @author Aaric, created on 2017-11-22T18:02.
+ * @since 2.0
+ */
+public class DataParserGmmcTest {
+
+    @Test
+    public void testPrint() {
+        System.out.println("hello");
+    }
+}
